b5f76d429ff0c3e408ff4a3ae2611a961b501962
Lcarney837 <landon.carney4@gmail.com>
2024-10-16 15:23:24 -0500

    Update ExamplePython.py
    Password deleted

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,5 +1,4 @@
-def test_password(x):
-    print(x)
+
 
 a = "String 1"
 b = "String 2"
@@ -19,6 +18,3 @@ job_list = {'Jhon': 'Doctor', 'Jane': 'Engineer','Jim': 'Teacher'}
 for name,job in job_list.items():
     print(f"{name}is a {job}")
 
-    password = 'SuperSecret'
-    test_password(password)
-    
